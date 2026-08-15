aa23d5b6083359791804e65acac2e6bd23131373
Navjot Singh <nafjot.singh2006@gmail.com>
2026-08-15 13:42:15 +0530

    [2130] [Time Beats: 41.32%] [Memory Beats: 79.21%] - Leet2Hub

---
+++ 02-Two-Pointers/2130. Maximum Twin Sum of a Linked List/2130. Maximum Twin Sum of a Linked List.c
@@ -0,0 +1,45 @@
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     struct ListNode *next;
+6 * };
+7 */
+8struct ListNode * reverseList(struct ListNode* head){
+9    struct ListNode * prev = NULL;
+10    struct ListNode * curr = head;
+11    struct ListNode * next_node = NULL;
+12
+13    while(curr!=NULL){
+14        next_node = curr->next;
+15        curr->next = prev;
+16        prev = curr;
+17        curr = next_node;
+18    }
+19
+20    return prev;
+21}
+22
+23int pairSum(struct ListNode* head) {
+24    struct ListNode * slow = head;
+25    struct ListNode * fast = head;
+26
+27    while(fast!=NULL){
+28        slow=slow->next;
+29        fast=fast->next->next;
+30    }
+31
+32    struct ListNode* new_head = reverseList(slow);
+33
+34    int maxSum = 0;
+35
+36    while(new_head!=NULL){
+37        if((head->val+new_head->val) > maxSum){
+38            maxSum = head->val+new_head->val;
+39        }
+40        head=head->next;
+41        new_head=new_head->next;
+42    }
+43
+44    return maxSum;
+45}
