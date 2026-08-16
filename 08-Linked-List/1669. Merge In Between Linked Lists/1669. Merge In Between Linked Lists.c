ddbd5b39ac62f7f29b9da5440f3b1263303259c4
Navjot Singh <nafjot.singh2006@gmail.com>
2026-08-16 17:02:31 +0530

    [1669] [Time Beats: 39.79%] [Memory Beats: 46.07%] - Leet2Hub

--- 08-Linked-List/1669. Merge In Between Linked Lists/1669. Merge In Between Linked Lists.c
+++ 08-Linked-List/1669. Merge In Between Linked Lists/1669. Merge In Between Linked Lists.c
@@ -1,33 +1,34 @@
 1/**
-2 * Definition for singly-linked list.
-3 * struct ListNode {
-4 *     int val;
-5 *     struct ListNode *next;
-6 * };
-7 */
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     struct ListNode *next;
+6 * };
+7 */
 8
 9
 10struct ListNode* mergeInBetween(struct ListNode* list1, int a, int b, struct ListNode* list2){
 11
-12    struct ListNode * node_before_a = list1;
+12    struct ListNode * start = list1;
 13    for(int i=1; i<a; i++){
-14        node_before_a=node_before_a->next;
+14        start=start->next;
 15    }
 16
-17    struct ListNode * current_node = node_before_a->next;
-18    for(int i=0; i < (b - a + 1); i++){
-19        struct ListNode* temp = current_node;
-20        current_node=current_node->next;
-21        free(temp);
-22    }
+17    struct ListNode * q = start->next;
+18    for(int i=0; i<(b-a+1); i++){
+19        q=q->next;
+20    }
+21
+22    struct ListNode * end = list2;
 23
-24    struct ListNode * list2_tail = list2;
-25    while(list2_tail->next != NULL){
-26        list2_tail = list2_tail->next;
-27    }
-28
-29    node_before_a->next=list2;
-30    list2_tail->next = current_node;
-31
-32    return list1;
-33}
+24    while(end->next != NULL){
+25
+26        end = end->next;
+27
+28    }
+29
+30    start->next=list2;
+31    end->next = q;
+32
+33    return list1;
+34}
