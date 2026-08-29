5faecaf37f9f71b2fdfaa81fbf7d2646a7c3e00f
Navjot Singh <nafjot.singh2006@gmail.com>
2026-08-29 22:32:43 +0530

    [2942] [Time Beats: 100.00%] [Memory Beats: 60.75%] - Leet2Hub

--- 01-Arrays-and-Hashing/2942. Find Words Containing Character/2942. Find Words Containing Character.c
+++ 01-Arrays-and-Hashing/2942. Find Words Containing Character/2942. Find Words Containing Character.c
@@ -6,17 +6,12 @@
 6    int size =0;
 7
 8    for(int i=0; i<wordsSize; i++){
-9        int j=0;
-10        while(words[i][j]!='\0'){
-11            if(words[i][j] == x){
-12                result[size++] = i;
-13                break;
-14            }
-15            j++;
-16        }
-17    }
-18
-19    *returnSize = size;
-20
-21    return result;
-22}
+9        if(strchr(words[i], x) != NULL){
+10            result[size++] = i;
+11        }
+12    }
+13
+14    *returnSize = size;
+15
+16    return result;
+17}
